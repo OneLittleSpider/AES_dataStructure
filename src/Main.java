65ae84b75d5819acb1848e8d54067b6eb53b6a12
OneLittleSpider <25jjian@athenian.org>
2024-12-03 09:16:50 -0800

    12/3 class work, finished implementin keyGenerator function

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,25 @@
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
+    public static SecretKey keyGenerator() throws NoSuchAlgorithmException{
+        KeyGenerator keyGenerator=KeyGenerator.getInstance("AES");
+        keyGenerator.init(128);
+        SecretKey key=keyGenerator.generateKey();
+        return key;
+
+    }
+
+
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Testing field for the AES!");
 
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
